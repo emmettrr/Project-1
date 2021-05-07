b336c0a2d08c0e4866c03e95a7369d5f2d747f12
Ted Keim <tedkeim@yahoo.com>
2021-05-06 21:20:51 -0400

    added header and nav bar

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,15 @@
 var client_id = "MjE4NjE5NTB8MTYyMDM0MjIxNC43MTMwOTUy"
+
+
+$(document).ready(function(){
+
+    $(".modal").modal();
+    $(".dropdown-button").dropdown();
+
+    $(".dropdown-button").on("click", () => {
+        console.log("button clicked")
+    })
+  })
+
+
+
