b1f684fb9d6c041adaa0ea85b06de682eb2597b2
SpeedRacer <robertgeorge2@gmail.com>
2021-05-13 10:53:26 -0400

    Hid both YouTube funcitons for testing. Found error that resulted in additonal fighers returned when a specific first name and last name were entered. Created var userChoiceFirst and userChoiceLast that both use string.split and .slice. Next cut the If statement to first check the data for both the userChoiceFirst AND userChoiceLast. ELSE IF check the rest of the original statement for userChoice in the first or the last names in the data. Tested locally and worked. Will continue to work on this and other updates to js and HTML to update dynamic page layout.

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,15 +13,80 @@ $("#submitBtn").on("click", function () {
         var last = data[i].LastName.toLowerCase();
         var userChoice = userInput.value.toLowerCase();
 
+        var userChoiceFirst = userChoice.split(' ').slice(0, -1).join(' '); //removes the last word, leaving first name
+        var userChoiceLast = userChoice.split(' ').slice(-1).join(' '); // removes the first word, leaving the last name
+
         if (userChoice.length === 0) {
           alert("select something!");
           return;
         }
 
-        if (
-          (first.includes(userChoice) && last.includes(userChoice)) ||
-          first.includes(userChoice) ||
-          last.includes(userChoice)
+        if (first.includes(userChoiceFirst) && last.includes(userChoiceLast)) {
+            console.log("First and Last Names:", userChoice);
+            console.log(data[i].FirstName);
+            console.log(data[i].LastName);
+  
+            var mmaFighter = document.createElement("div");
+            var firstName = document.createElement("li");
+            var lastName = document.createElement("li");
+            var nickName = document.createElement("li");
+            var birthDate = document.createElement("li");
+            var height = document.createElement("li");
+            var weight = document.createElement("li");
+            var wins = document.createElement("li");
+            var losses = document.createElement("li");
+  
+            mmaFighter.append(
+              firstName,
+              lastName,
+              nickName,
+              birthDate,
+              height,
+              weight,
+              wins,
+              losses
+            );
+            mmaFighter.classList.add("card-content");
+            firstName.textContent = "First Name: ";
+            lastName.textContent = "Last Name: ";
+            nickName.textContent = "Nick Name: ";
+            birthDate.textContent = "Age: ";
+            height.textContent = "Height: ";
+            weight.textContent = "Weight: ";
+            wins.textContent = "Wins: ";
+            losses.textContent = "Losses: ";
+            mmaFighter;
+  
+            var fName = data[i].FirstName;
+            firstName.append(fName);
+  
+            var lName = data[i].LastName;
+            lastName.append(lName);
+  
+            var nName = data[i].Nickname;
+            nickName.append(nName);
+  
+            var bDate = data[i].BirthDate.substring(0, 4);
+            var bDateFix = Math.floor(2021 - bDate);
+            birthDate.append(bDateFix);
+  
+            var ht = data[i].Height;
+            var htFix = Math.floor(ht / 12);
+            height.append(htFix);
+  
+            var wt = data[i].Weight;
+            weight.append(wt);
+  
+            var ws = data[i].Wins;
+            wins.append(ws);
+  
+            var ls = data[i].Losses;
+            losses.append(ls);
+  
+            mmaList.append(mmaFighter);
+
+          } else if ( 
+            first.includes(userChoice) || last.includes(userChoice)
         ) {
           console.log(data[i].FirstName);
           console.log(data[i].LastName);
@@ -94,51 +159,51 @@ $("#submitBtn").on("click", function () {
   localStorage.getItem(userInput.value);
 });
 
-$(document).ready(() => {
-  const doSearch = () => {
-    let searchQuery = $(".search input:text").val();
-    let url =
-      "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&key=AIzaSyAr031YDIiLF3eBObxe03kuBL5aLwA-8XQ&q=" +
-      searchQuery;
-    $.ajax({
-      url: url,
-      method: "GET",
-      success: (result) => {
-        $(".video-play").text("");
-        $(".video-play").append(
-          `<iframe class="embed-responsive-item" src=https://www.youtube.com/embed/${result.items[0].id.videoId} allowFullScreen title='youtube player' />`
-        );
-        populateSuggestions(result.items.slice(1, 10));
-      },
-      error: (err, response) => {
-        console.log(err.responseText);
-        $(".video-play").text(err.responseText);
-      },
-    });
-  };
+// $(document).ready(() => {
+//   const doSearch = () => {
+//     let searchQuery = $(".search input:text").val();
+//     let url =
+//       "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&key=AIzaSyAr031YDIiLF3eBObxe03kuBL5aLwA-8XQ&q=" +
+//       searchQuery;
+//     $.ajax({
+//       url: url,
+//       method: "GET",
+//       success: (result) => {
+//         $(".video-play").text("");
+//         $(".video-play").append(
+//           `<iframe class="embed-responsive-item" src=https://www.youtube.com/embed/${result.items[0].id.videoId} allowFullScreen title='youtube player' />`
+//         );
+//         // populateSuggestions(result.items.slice(1, 10));
+//       },
+//       error: (err, response) => {
+//         console.log(err.responseText);
+//         $(".video-play").text(err.responseText);
+//       },
+//     });
+//   };
 
-  const populateSuggestions = (videos) => {
-    $(".suggest-list").text("");
-    for (video of videos) {
-      let videoElement = `<a href="#" class="suggested" data-videoId=${video.id.videoId} ><img src=${video.snippet.thumbnails.medium.url} /></a>`;
-      $(".suggest-list").append(videoElement);
-    }
+  // const populateSuggestions = (videos) => {
+  //   $(".suggest-list").text("");
+  //   for (video of videos) {
+  //     let videoElement = `<a href="#" class="suggested" data-videoId=${video.id.videoId} ><img src=${video.snippet.thumbnails.medium.url} /></a>`;
+  //     $(".suggest-list").append(videoElement);
+  //   }
 
-    $("a.suggested").click((e) => {
-      let videoId = e.currentTarget.dataset.videoId;
-      $(".video-play").text("");
-      $(".video-play").append(
-        `<iframe class="embed-responsive-item" src=https://www.youtube.com/embed/${videoId} allowFullScreen title='youtube player' />`
-      );
-    });
-  };
+  //   $("a.suggested").click((e) => {
+  //     let videoId = e.currentTarget.dataset.videoId;
+  //     $(".video-play").text("");
+  //     $(".video-play").append(
+  //       `<iframe class="embed-responsive-item" src=https://www.youtube.com/embed/${videoId} allowFullScreen title='youtube player' />`
+  //     );
+  //   });
+  // };
 
-  $("button:button").click(() => {
-    doSearch();
-  });
+//   $("button:button").click(() => {
+//     doSearch();
+//   });
 
-  doSearch();
-});
+//   doSearch();
+// });
 
 const Http = new XMLHttpRequest();
 const url =
